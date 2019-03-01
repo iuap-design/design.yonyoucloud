f0f2f99aa0c750b397c1ddb956ff61c6485985fb
liushaozhen <804199994@qq.com>
2019-03-01 15:43:43 +0800

    设计语言

---
+++ ucf-apps/design-lang/src/indexView/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import './index.less';
+
+class Index extends Component {
+    render() { 
+        return (
+            <div className="home" >
+                <NavBar/>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default Index;
