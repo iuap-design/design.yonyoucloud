561873ef9703a85bd5f8810754a77f212104bb9c
guoxj9966 <guo_xue_jing@163.com>
2019-03-02 10:01:54 +0800

    footer

--- ucf-apps/design/src/components/RichCase/index.js
+++ ucf-apps/design/src/components/RichCase/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { actions } from 'mirrorx';
 import { Button } from 'tinper-bee';
+import richcase from 'images/richcase.png';
 //import {AcButton} from 'ac-button';
 import './index.less';
 
@@ -9,8 +10,11 @@ class RichCase extends Component {
     render() { //包含事件按钮
         return (
            <div className="richcase-content">
-                <p className="richcase-title">丰富案例覆盖典型业务场景</p>
-                 <Button className="richcase-btn"  onClick={this.handleClick}>查看案例</Button>
+                <img className="pic" src={richcase}/>
+                <div className="para1" height="288px"></div>
+                <div className="part2" ></div>
+                {/* <p className="richcase-title">丰富案例覆盖典型业务场景</p>
+                 <Button className="richcase-btn"  onClick={this.handleClick}>查看案例</Button> */}
             </div>
         );
     }
--- ucf-apps/design/src/indexView/index.js
+++ ucf-apps/design/src/indexView/index.js
@@ -5,11 +5,12 @@ import LanguageAbility from '../components/LanguageAbility';
 import ComponentRange from '../components/ComponentRange';
 import RichCase from '../components/RichCase';
 import DevelopTools from '../components/DevelopTools';
-import Footer from '../../../../ucf-common/src/components/Footer';
+//import Footer from '../../../../ucf-common/src/components/Footer';
+import Footer from 'components/Footer';
 import './index.less';
 
 class Index extends Component {
-    render() { //包含事件按钮 ss
+    render() { 
         return (
             <div className="home" >
                 <NavBar/>
