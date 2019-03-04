1574a470ba2a4f0e5d292758f0f0a5d07829f533
liushaozhen <804199994@qq.com>
2019-03-04 14:55:45 +0800

    地址

--- ucf-common/src/components/NavBar/index.js
+++ ucf-common/src/components/NavBar/index.js
@@ -24,15 +24,13 @@ class NavBar extends Component {
                 <Col className='nav-right' md={7}>
                     <ul className="nav-options">
                         <li><a href="">首页</a></li>
-                        <li><a target='_blank' href="//www.yuque.com/mo-college/vis-design/krfr2r">设计语言</a></li>
+                        <li><a href="">设计语言</a></li>
                         <li><a href="">基础组件</a></li>
                         <li><a href="">应用组件</a></li>
                         <li><a href="">典型案例</a></li>
                         <li>
                             <Select defaultValue="zh_CN" dropdownClassName='nav-lang-select'>
                                 <Option value="zh_CN">简体中文</Option>
-                                <Option value="zh_TW">繁体中文</Option>
-                                <Option value="en_US">英文</Option>
                             </Select>
                         </li>
                     </ul>  
