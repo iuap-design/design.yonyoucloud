b00f280815eda1a7454c957e3cc16376ceb7d8cd
liushaozhen <804199994@qq.com>
2019-03-04 14:43:12 +0800

    自适应修改

--- ucf-common/src/components/Footer/index.js
+++ ucf-common/src/components/Footer/index.js
@@ -9,86 +9,90 @@ import time from 'images/time.png';
 class Footer extends Component {
     render() { //包含事件按钮 
         return (
-            <Row className="footer">
-                <Col md={3} xs={12} sm={6} className="footer-top-item">
-                    <h5>相关资源</h5>
-                    <ul>
-                        <li>
-                            <a href="https://mock.yonyoucloud.com/">Mock 接口管理平台</a>
-                        </li>
-                        <li>
-                            <a href="https://package.yonyoucloud.com/#/">YNPM-快速、安全、可统计的 包管理平台</a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/tinper-acs/ac-tools">ac-tools 业务组件开发工具</a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/tinper-acs/ac-tools">YYIMSDK 即时通讯 SDK</a>
-                        </li>
-                        <li>
-                            <a href="https://www.yuque.com/ucf-web/book">UCF-WEB 微前端框架</a>
-                        </li>
-                        <li>
-                            <a href="http://tinper.org/webide/">Moy 框架在线示例</a>
-                        </li>
-                    </ul>
-                </Col>
-                <Col className="footer-top-item" md={3} xs={12} sm={6}>
-                    <h5>社区</h5>
+            <div className="footer">
+                <Row className='footer-top'>
+                    <Col className="footer-top-item" md={3} xs={12} sm={6} >
+                        <h5>相关资源</h5>
                         <ul>
                             <li>
-                                <a href="https://github.com/iuap-design/blog/issues">云平台体验技术团队BLOG</a>
+                                <a href="https://mock.yonyoucloud.com/">Mock 接口管理平台</a>
                             </li>
                             <li>
-                                <a href="">tinper 官方技术交流群</a>
+                                <a href="https://package.yonyoucloud.com/#/">YNPM-快速、安全、可统计的 包管理平台</a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/tinper-acs/ac-tools">ac-tools 业务组件开发工具</a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/tinper-acs/ac-tools">YYIMSDK 即时通讯 SDK</a>
+                            </li>
+                            <li>
+                                <a href="https://www.yuque.com/ucf-web/book">UCF-WEB 微前端框架</a>
+                            </li>
+                            <li>
+                                <a href="http://tinper.org/webide/">Moy 框架在线示例</a>
                             </li>
                         </ul>
-                </Col>
-                <Col className="footer-top-item" md={3} xs={12} sm={6}>
-                    <h5>帮助</h5>
-                    <ul>
-                        <li>
-                            <a href="https://github.com/tinper-bee">组件Github 源码</a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/iuap-design/tinper-bee/releases">更新记录</a>
-                        </li>
-                        <li>
-                            <a href="">常见问题</a>
-                        </li>
-                    </ul>
-                </Col>
-                <Col className="footer-top-item" md={3} xs={12} sm={6}>
-                    <h5>联系我们</h5>
+                    </Col>
+                    <Col className="footer-top-item" md={3} xs={12} sm={6}>
+                        <h5>社区</h5>
+                            <ul>
+                                <li>
+                                    <a href="https://github.com/iuap-design/blog/issues">云平台体验技术团队BLOG</a>
+                                </li>
+                                <li>
+                                    <a href="">tinper 官方技术交流群</a>
+                                </li>
+                            </ul>
+                    </Col>
+                    <Col className="footer-top-item" md={3} xs={12} sm={6}>
+                        <h5>帮助</h5>
                         <ul>
                             <li>
-                            <img src={phone}/> 400-6815456
-                            </li>
-                            <li><img src={mail}/> zhaoyni@yonyou.com</li>
-                            <li>
-                                <img src={search}/> 北京市海淀区北清路68号用友产业园iuap事业部
+                                <a href="https://github.com/tinper-bee">组件Github 源码</a>
                             </li>
                             <li>
-                                <img src={time}/> 周一至周五9:00-17:00
+                                <a href="https://github.com/iuap-design/tinper-bee/releases">更新记录</a>
+                            </li>
+                            <li>
+                                <a href="">常见问题</a>
                             </li>
                         </ul>
-                </Col>
-                <Col className="footer-middle" md={12} xs={12} sm={12} >
-                    <div className="footer-line">
-                    </div>
-                </Col>
-                <Col className="footer-base" md={12}  xs={12} sm={12}>
-                    <div className="footer-base-font">
-                        <p>
-                            版权所有：用友网络科技股份有限公司 2016 京ICP备05007539号-7 京ICP证100714号 京公网安备1101080209224号
-                        </p>
-                        <p>
-                            Made with by FED team
-                        </p>
-                    </div>
-                    
-                </Col>
-            </Row>
+                    </Col>
+                    <Col className="footer-top-item" md={3} xs={12} sm={6}>
+                        <h5>联系我们</h5>
+                            <ul>
+                                <li>
+                                <img src={phone}/> 400-6815456
+                                </li>
+                                <li><img src={mail}/> zhaoyni@yonyou.com</li>
+                                <li>
+                                    <img src={search}/> 北京市海淀区北清路68号用友产业园iuap事业部
+                                </li>
+                                <li>
+                                    <img src={time}/> 周一至周五9:00-17:00
+                                </li>
+                            </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="footer-middle" md={12} xs={12} sm={12} >
+                        <div className="footer-line">
+                        </div>
+                    </Col>
+                    <Col className="footer-base" md={12}  xs={12} sm={12}>
+                        <div className="footer-base-font">
+                            <p>
+                                版权所有：用友网络科技股份有限公司 2016 京ICP备05007539号-7 京ICP证100714号 京公网安备1101080209224号
+                            </p>
+                            <p>
+                                Made with by FED team
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            
         );
     }
 }
