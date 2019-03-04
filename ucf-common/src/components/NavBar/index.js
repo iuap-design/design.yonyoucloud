d75c5c030a55a9ad9d0a47bd45717fcaf9f4f84e
liushaozhen <804199994@qq.com>
2019-03-04 14:55:02 +0800

    设计语言地址

--- ucf-common/src/components/NavBar/index.js
+++ ucf-common/src/components/NavBar/index.js
@@ -24,7 +24,7 @@ class NavBar extends Component {
                 <Col className='nav-right' md={7}>
                     <ul className="nav-options">
                         <li><a href="">首页</a></li>
-                        <li><a href="">设计语言</a></li>
+                        <li><a target='_blank' href="//www.yuque.com/mo-college/vis-design/krfr2r">设计语言</a></li>
                         <li><a href="">基础组件</a></li>
                         <li><a href="">应用组件</a></li>
                         <li><a href="">典型案例</a></li>
