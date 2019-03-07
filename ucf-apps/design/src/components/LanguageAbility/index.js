80a8220c276dbf16f7750fe23a72bd5321650e61
guoxj9966 <guo_xue_jing@163.com>
2019-03-07 15:51:05 +0800

    DevelopTools

--- ucf-apps/design/src/components/LanguageAbility/index.js
+++ ucf-apps/design/src/components/LanguageAbility/index.js
@@ -15,13 +15,13 @@ class LanguageAbility extends Component {
                         </p>
                     </div>  
                 </Col>
-                <Col md={12}>
+                {/* <Col md={12}>
                     <div>
                         <p className="lang-content">
                             iuap design 是一个企业级前端集成解决方案，基于开源前端技术平台tinper上，给用户提供前端模型框架kero、在线模板中心、强大的在线可视化页面设计器以及丰富的企业应用开发架构方案，并且提供技术咨询和技术培训，帮助用户快速构建应用。
                         </p>
                     </div>
-                </Col>
+                </Col> */}
                 <Col className="lang-base-first" md={3} xs={6} sm={6}>
                     <div>
                          <div class="white-radius"></div>
