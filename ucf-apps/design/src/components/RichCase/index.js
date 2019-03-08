ff420c38a34aa60059a42ab00885634442c561af
guoxj9966 <guo_xue_jing@163.com>
2019-03-09 07:59:08 +0800

    导航栏图标和RichCase组件的修改改改

--- ucf-apps/design/src/components/RichCase/index.js
+++ ucf-apps/design/src/components/RichCase/index.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import { actions } from 'mirrorx';
 import { Button } from 'tinper-bee';
 import richcase from 'images/richcase.png';
+import richback from 'images/richback.png';
+import {Row,Col} from 'tinper-bee';
+import png1 from 'images/richback.png';
 import './index.less';
+console.log('pnhg1')
+// const bground={
+//     display:'inline-block',
+//     backgroudImage:'url('+images/richback.png+')'h
+// };
 
-
+// const bgGround={
+//     display:'inline-block',
+//     background: `url(${require("images/richback.png")})`
+// }
 class RichCase extends Component {
     render() {
         return (
-           <div className="richcase-content">
-                <div className="para1"/>
-                <div className="img-div" >
-                    <img className="pic" src={richcase}/>
-                </div>
-                <div className="richcase-text">
+            <Row className="richcontent" style={{backgroundImage:`url(${png1})`}}>
+            {/* <Row className="richcontent" style={bgGround}> */}
+               <Col  className="img-div" md={7} >
+                  <img className="pic" src={richcase}/>
+               </Col>
+               <Col className="richcase-text" md={5}>
                     <p className="richcase-title">丰富案例覆盖典型业务场景</p>
                     <Button className="richcase-btn"  onClick={()=>{window.location.href="https://design.yonyoucloud.com/case/preview"}}>查看案例</Button>
-                </div>
-            </div>
+               </Col>
+            </Row>
         );
     }
 }
+
+
+// class RichCase extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <div className="richcase-content">
+//                      <div className="para1"/>
+//                       <div className="img-div" >
+//                             <img className="pic" src={richcase}/>
+//                        </div>
+//                       <div className="richcase-text">
+//                             <p className="richcase-title">丰富案例覆盖典型业务场景</p>
+//                             <Button className="richcase-btn"  onClick={()=>{window.location.href="https://design.yonyoucloud.com/case/preview"}}>查看案例</Button>
+//                        </div>
+//                 </div>
+//                 <div className="mob-richcase-content">
+//                     <div className="pic">
+//                         <img className="pic" src={richcase}/>
+//                     </div>
+//                     <div className="content">
+//                         <p className="richcase-title">丰富案例覆盖典型业务场景</p>
+//                         <Button className="richcase-btn"  onClick={()=>{window.location.href="https://design.yonyoucloud.com/case/preview"}}>查看案例</Button>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+
+
 
 export default RichCase;
