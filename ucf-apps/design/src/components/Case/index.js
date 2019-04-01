bfdc29b3e3d9895a9923f4f4041687f5db38d976
liushaozhen <804199994@qq.com>
2019-04-01 18:32:30 +0800

    css

--- ucf-apps/design/src/components/Case/index.js
+++ ucf-apps/design/src/components/Case/index.js
@@ -7,33 +7,33 @@ import './index.less';
 const baseUrl = '//design.yonyoucloud.com/static/tinper-bee/logo/'
 
 const imgs={
+    '中兴通讯':baseUrl+'中兴通讯'+'.png',
+    '三一重工':baseUrl+'三一重工'+'.png',
+    '台湾华新丽华-MES 系统':baseUrl+'台湾华新丽华-MES 系统'+'.png',
+    '阳光保险':baseUrl+'阳光保险'+'.png',
+    '象屿集团':baseUrl+'象屿集团'+'.png',
     '杭州路桥':baseUrl+'杭州路桥'+'.png',
     '杭州绿城':baseUrl+'杭州绿城'+'.png',
     '江铜贵溪冶炼厂-智能工厂':baseUrl+'江铜贵溪冶炼厂-智能工厂'+'.png',
     '郎酒CRM平台':baseUrl+'郎酒CRM平台'+'.png',
     '南通化轻物流系统':baseUrl+'南通化轻物流系统'+'.png',
     '全友数字营销系统':baseUrl+'全友数字营销系统'+'.png',
-    '三一重工':baseUrl+'三一重工'+'.png',
     '尚层装饰':baseUrl+'尚层装饰'+'.png',
     '四维图新':baseUrl+'四维图新'+'.png',
-    '台湾华新丽华-MES 系统':baseUrl+'台湾华新丽华-MES 系统'+'.png',
-    '象屿集团':baseUrl+'象屿集团'+'.png',
-    '阳光保险':baseUrl+'阳光保险'+'.png',
-    '用友网络-工单系统':baseUrl+'用友网络-工单系统'+'.png',
+    '用友网络nccloud':baseUrl+'用友网络nccloud'+'.png',
+    '用友网络-U8Cloud 数据中心':baseUrl+'用友网络-U8Cloud 数据中心'+'.png',
     '用友网络-精智IOT 后台管理系统':baseUrl+'用友网络-精智IOT 后台管理系统'+'.png',
     '用友网络-开发者中心':baseUrl+'用友网络-开发者中心'+'.png',
+    '用友网络-专属云应用平台':baseUrl+'用友网络-专属云应用平台'+'.png',
     '用友网络-开放平台':baseUrl+'用友网络-开放平台'+'.png',
     '用友网络-企业账号中心':baseUrl+'用友网络-企业账号中心'+'.png',
     '用友网络-微服务治理平台':baseUrl+'用友网络-微服务治理平台'+'.png',
     '用友网络-用友云平台官网':baseUrl+'用友网络-用友云平台官网'+'.png',
     '用友网络-云平台 UI 模板':baseUrl+'用友网络-云平台 UI 模板'+'.png',
-    '用友网络-专属云应用平台':baseUrl+'用友网络-专属云应用平台'+'.png',
     '用友网络-diwork 工作台':baseUrl+'用友网络-diwork 工作台'+'.png',
     '用友网络-NC 工程云':baseUrl+'用友网络-NC 工程云'+'.png',
     '用友网络-NC 司库云':baseUrl+'用友网络-NC 司库云'+'.png',
-    '用友网络-U8Cloud 数据中心':baseUrl+'用友网络-U8Cloud 数据中心'+'.png',
-    '用友网络nccloud':baseUrl+'用友网络nccloud'+'.png',
-    '中兴通讯':baseUrl+'中兴通讯'+'.png',
+    '用友网络-工单系统':baseUrl+'用友网络-工单系统'+'.png',
 }
 const toLogos=()=>{
     let logos={},key=0;
@@ -73,6 +73,7 @@ class Case extends Component {
         this.setState({
             active
         })
+        let length = Object.keys(logos).length;
         this.timeOut=setInterval(()=>{
             if(active==length-1){
                 active=0;
--- ucf-apps/design/src/components/LanguageAbility/index.js
+++ ucf-apps/design/src/components/LanguageAbility/index.js
@@ -14,25 +14,25 @@ class LanguageAbility extends Component {
                 </Col>
                 <Col className="lang-base-first" md={3} xs={12} sm={12}>
                     <div>
-                         <div className="white-radius"></div>
+                         <img className="white-radius" src="//design.yonyoucloud.com/static/tinper-bee/logo/LanguageAbility1.png"/>
                          <p style={{fontSize:20}}>明确清晰</p>
                     </div>
                 </Col>
                 <Col className="lang-base-second"  md={3} xs={12} sm={12}>
                     <div>
-                       <div className="white-radius"></div>
+                    <img className="white-radius" src="//design.yonyoucloud.com/static/tinper-bee/logo/LanguageAbility2.png"/>
                         <p  style={{fontSize:20}}>灵活简易</p>
                     </div>
                 </Col>
                 <Col className="lang-base-third" md={3} xs={12} sm={12}>
                     <div>
-                        <div className="white-radius"></div>
+                    <img className="white-radius" src="//design.yonyoucloud.com/static/tinper-bee/logo/LanguageAbility3.png"/>
                         <p style={{fontSize:20}}>标准一致</p>
                     </div>
                 </Col>
                 <Col className="lang-base-forth" md={3} xs={12} sm={12}>
                     <div>
-                        <div className="white-radius"></div>
+                    <img className="white-radius" src="//design.yonyoucloud.com/static/tinper-bee/logo/LanguageAbility4.png"/>
                         <p style={{fontSize:20}}>活力美观</p>
                     </div>
                 </Col>
