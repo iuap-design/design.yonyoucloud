d4477600c96b5a47a50755c7510f0646f62fa362
guoxj9966 <guo_xue_jing@163.com>
2019-04-26 17:05:52 +0800

    首页丰富案例

--- ucf-apps/design/src/components/RichCase/index.js
+++ ucf-apps/design/src/components/RichCase/index.js
@@ -1,36 +1,67 @@
 import React, { Component } from 'react';
 import { Button } from 'tinper-bee';
-import richcase from 'images/richcase.png';
-import richback from 'images/richback.png';
 import {Row,Col} from 'tinper-bee';
-import png1 from 'images/richback.png';
+import bg from 'images/bg.png';
+import pic1 from 'images/pic1.png';
+import pic2 from 'images/pic2.png';
+import pic3 from 'images/pic3.png';
+import pic4 from 'images/pic4.png';
+import pic5 from 'images/pic5.png';
+import Carousel from "tinper-bee/lib/Carousel";
 import './index.less';
 class RichCase extends Component {
     render() {
+        var imgStyle = {
+            display:"inline-block",
+            width: "937px",
+            height: "552px",
+            background: `url("${bg}") center center / cover no-repeat`
+        };
+        const params = {
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'bullets',
+              clickable: true
+            },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev'
+            },
+            spaceBetween: 30
+            // effect: 'flip' //设置3d翻转
+          }
         return (
-            <div>
-                <Row className="richcontent" style={{backgroundImage:`url(${png1})`}}>
-                {/* <Row className="richcontent" style={bgGround}> */}
-                    <Col className="img-div" md={7} >
-                         <img className="pic" src={richcase}/>
-                    </Col>
-                    <Col className="richcase-text" md={5}>
-                        <p className="richcase-title">丰富案例覆盖典型业务场景</p>
-                        <Button className="richcase-btn" >
-                            <a href="https://design.yonyoucloud.com/case/preview" target="_blank">查看案例</a>
-                        </Button>
-                    </Col>
-                </Row>
-                <div className="mob-richcontent">
-                       <div className="img-div">
-                           <img className="pic" src={richcase}/>
-                       </div>
-                       <div className="richcase-text">
-                           <p className="richcase-title">丰富案例覆盖典型业务场景</p>
-                           <Button className="richcase-btn" >
-                                <a href="https://design.yonyoucloud.com/case/preview" target="_blank">查看案例</a>
-                           </Button>
-                       </div>
+            <div className="richcase">
+                <div className="content">
+                    <div className="title">丰富案例覆盖典型业务场景</div>
+                    <Button className="btn" >
+                        <a href="https://design.yonyoucloud.com/case/preview" target="_blank">查看案例</a>
+                    </Button>
+                </div>
+                <div className="img" style={imgStyle}>
+                    <Carousel {...params}>
+                    {/* 封装成组件 */}
+                        <div className="swiper-pic">
+                            <span>单表筛选</span>
+                            <img src={pic1}/>
+                        </div>
+                        <div className="swiper-pic">
+                            <span>卡表</span>
+                            <img src={pic2}/>
+                        </div>
+                        <div className="swiper-pic">
+                            <span>树表</span>
+                            <img src={pic3}/>
+                        </div>
+                        <div className="swiper-pic">
+                            <span>一主多子</span>
+                            <img src={pic4}/>
+                        </div>
+                        <div className="swiper-pic">
+                            <span>主子表</span>
+                            <img src={pic5}/>
+                        </div>
+                    </Carousel>
                 </div>
             </div>
         );
