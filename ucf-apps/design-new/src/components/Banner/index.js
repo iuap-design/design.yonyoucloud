debf97b8863174eb3548381f41b3235dcadfe980
liushaozhen <804199994@qq.com>
2019-05-29 10:07:06 +0800

    移动端css

--- ucf-apps/design-new/src/components/Banner/index.js
+++ ucf-apps/design-new/src/components/Banner/index.js
@@ -6,31 +6,31 @@ import './index.less';
 class Banner extends Component {
     render() { 
         return (
-            <div  style={{'height':1110}}>
-                <div className="banner" style={{'height':930}}>
-                    <div className="header">
-                        <span className="header-left">
-                            <a href='#' className="index">首页</a>
-                            <a target='_blank' href='https://github.com/iuap-design/blog/issues'>技术博客</a>
-                        </span>
-                        <span className="header-right">
-                            <a target='_blank' href="https://github.com/iuap-design/tinper-bee">Github</a>
-                        </span>
+            <div  className="banner">
+                <div className="header">
+                    <span className="header-left">
+                        <a href='#' className="index">首页</a>
+                        <a target='_blank' href='https://github.com/iuap-design/blog/issues'>技术博客</a>
+                    </span>
+                    <span className="header-right">
+                        <a target='_blank' href="https://github.com/iuap-design/tinper-bee">Github</a>
+                    </span>
+                </div>
+                <div className="left-text">
+                    <div className="tinper">
+                        <a target='_blank' href='/'>
+                            <img src="//tinper.org/assets/images/tinperlogos%2060w.png"/>
+                        </a>
                     </div>
-                    <div className="left-text">
-                        <div className="tinper">
-                            <a target='_blank' href='/'>
-                                <img src="//tinper.org/assets/images/tinperlogos%2060w.png"/>
-                            </a>
-                        </div>
-                        <div className="tinper-title">
-                            企业级大前端基础设施平台
-                        </div>
-                        <div className="tinper-ctn">
-                            为数字化企业实现多终端快速开发及产品极致体验提供一站式解决方案
-                        </div>
+                    <div className="tinper-title">
+                        企业级大前端基础设施平台
                     </div>
-                    <div className="design">
+                    <div className="tinper-ctn">
+                        为数字化企业实现多终端快速开发及产品极致体验提供一站式解决方案
+                    </div>
+                </div>
+                <img className='img-bg' src='//design.yonyoucloud.com/static/tinper-bee/logo/banner.png'></img>
+                <div className="design">
                         <Title title="为数字化企业而生的体验能力"/>
                         <Row>
                             <Col className="design-item" md={4} xs={12}>
@@ -74,7 +74,6 @@ class Banner extends Component {
                             </Col>
                         </Row>
                     </div>
-                </div>
             </div>
         );
     }
