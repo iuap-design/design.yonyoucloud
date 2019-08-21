2cb443da9b14f4233953872d56e76f96ef7a5501
yangchch6 <yangchch6@yonyou.com>
2019-08-21 18:49:12 +0800

    2.1.6

--- ucf.config.js
+++ ucf.config.js
@@ -30,7 +30,7 @@ module.exports = (env, argv) => {
         },
         // 全局环境变量
         global_env: {
-            TINPERVERSION:JSON.stringify("V2.1.5")
+            TINPERVERSION:JSON.stringify("V2.1.6")
         },
         // 别名配置
         alias: {
