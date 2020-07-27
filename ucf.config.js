76a4d6069aa52beef19b64754bb2c95bc041728c
liushaozhen <804199994@qq.com>
2020-07-27 17:28:59 +0800

    2.3.9

--- ucf.config.js
+++ ucf.config.js
@@ -30,7 +30,7 @@ module.exports = (env, argv) => {
         },
         // 全局环境变量
         global_env: {
-            TINPERVERSION:JSON.stringify("V2.3.8")
+            TINPERVERSION:JSON.stringify("V2.3.9")
         },
         // 别名配置
         alias: {
